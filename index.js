2bd3c728be14f5bebdb8b5a54e936c2821920d70
Jirne <j.pdh76@gmail.com>
2023-11-07 08:29:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "z":
+            break
+
+        case "s":
+            break
+
+        case "q":
+            break
+
+        case "d":
+            break
+    }
+})
