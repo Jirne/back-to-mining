0b68cc3983760f00844cd4aed8a24f2e486def4e
Jirne <j.pdh76@gmail.com>
2023-12-07 22:55:50 +0100

    src directory + correction code

--- src/reusable/Camera.js
+++ src/reusable/Camera.js
@@ -1,20 +1,24 @@
+import Game from "./Game.js";
+
 export default class Camera {
 
-    constructor({coordinates}){
+    constructor({ coordinates }) {
         this.coordinates = coordinates
     }
 
-    draw(){
-        const c = document.getElementById("playground").getContext("2d")
+    draw(game) {
+        /*
+        const c = game.context["playground"]
         c.fillStyle = "black"
         c.rect(this.coordinates.x, this.coordinates.y, this.coordinates.w, this.coordinates.h);
         c.stroke();
+        */
     }
 
-    isPlayerInside(coordinates){
+    isPlayerInside(coordinates) {
         return (coordinates.x > this.coordinates.x &&
             coordinates.x + coordinates.w < this.coordinates.x + this.coordinates.w &&
-            coordinates.y  > this.coordinates.y &&
+            coordinates.y > this.coordinates.y &&
             coordinates.y + coordinates.h < this.coordinates.y + this.coordinates.h)
     }
 }
