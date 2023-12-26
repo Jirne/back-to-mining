c0ecebedce0639dee9b7a50e4ecb73b96f8ee8a8
Jirne <j.pdh76@gmail.com>
2023-12-26 18:47:20 +0100

    the second file maybe ?

--- src/back-to-mining/reusable/Game.js
+++ src/back-to-mining/reusable/Game.js
@@ -38,7 +38,7 @@ export default class Game {
           console.log(`${url} loaded`)
           resolve(img)
         };
-        img.src = '/src//back-to-mining/assets/' + url;
+        img.src = '../assets/' + url;
         this.assets.set(url.slice(url.lastIndexOf("/") + 1, url.length - 4), img)
       })
     );
