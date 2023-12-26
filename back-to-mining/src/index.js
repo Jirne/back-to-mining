8fa6447f27ed81865b603ee17e15108abc5128bc
Jirne <j.pdh76@gmail.com>
2023-12-26 18:51:15 +0100

    delete useless folder

--- back-to-mining/src/index.js
+++
@@ -1,8 +0,0 @@
-import { MyGame } from "./classes/MyGame.js"
-
-const myGame = new MyGame(1280, 60, 16, [
-    'TileSet.png'
-], [
-    { "name": "background", "zIndex": 0 },
-    { "name": "playground", "zIndex": 1 }
-])
